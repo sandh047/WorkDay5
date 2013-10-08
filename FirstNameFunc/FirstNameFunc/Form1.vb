a3ad3558d64c2722ee68ceb4d7be6512e7825d18
sandh047 <wsandholm@yahoo.com>
2013-10-07 23:05:46 -0500

    Work Day 5

--- FirstNameFunc/FirstNameFunc/Form1.vb
+++ FirstNameFunc/FirstNameFunc/Form1.vb
@@ -5,11 +5,17 @@ Public Class Form1
     Private Sub btnDetermine_Click(sender As System.Object, e As System.EventArgs) Handles btnDetermine.Click
         Dim name As String
         name = txtFullName.Text
+
         txtFirstName.Text = FirstName(name)
     End Sub
 
     Function FirstName(ByVal name As String) As String
-        Return "Not Implemented Yet"
+
+        name = name.Trim()
+        Dim index As Integer = name.IndexOf(" ")
+        Dim fName As String = name.Substring(0, index)
+        Return fName
+
     End Function
 
 End Class
--- RightTriangle/RightTriangle/Form1.vb
+++ RightTriangle/RightTriangle/Form1.vb
@@ -4,8 +4,16 @@ Public Class Form1
 
     Private Sub btnCalculate_Click(sender As System.Object, e As System.EventArgs) Handles btnCalculate.Click
 
-        txtHyp.Text = "Not Implemented Yet"
+        If IsNumeric(txtSideOne.Text) = True And IsNumeric(txtSideTwo.Text) = True Then
+            txtHyp.Text = hypotenuse(CDbl(txtSideOne.Text), CDbl(txtSideTwo.Text))
+        Else
+            MessageBox.Show("Please enter in valid values")
+        End If
 
     End Sub
+
+    Function hypotenuse(ByVal sideOne As Double, ByVal sideTwo As Double) As String
+        Return CStr(Math.Sqrt(Math.Pow(CDbl(txtSideOne.Text), 2.0) + Math.Pow(CDbl(txtSideTwo.Text), 2.0)))
+    End Function
 
 End Class
